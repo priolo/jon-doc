5e670a88f2c441317018adfa28a44b6852b8aeb9
ivano.iorio <ivano.iorio@zerofarms.it>
2025-12-27 17:31:40 +0100

    chore: update project name in Docusaurus config to 'jon-doc'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'priolo', // Usually your GitHub org/user name.
-  projectName: 'priolo.github.io/test-doc', // Usually your repo name.
+  projectName: 'jon-doc', // Usually your repo name.
   trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
